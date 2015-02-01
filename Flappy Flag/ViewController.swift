22ff83b27ee755eac5b8369b7206e755d6c00f1c
Zane Shannon <zcs@smileslaughs.com>
2015-02-01 14:54:18 -0800

    added flag loading to example app. #3

--- Flappy Flag/ViewController.swift
+++ Flappy Flag/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var flag:UIImageView?
 	@IBOutlet var label:UILabel?
 	@IBOutlet var sublabel:UILabel?
 	
@@ -17,8 +18,18 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		WhereYat.locate { (location, error) in
 			if (error == nil) {
-				self.label!.text = location?.city
-				self.sublabel!.text = location?.country
+				self.label!.text = location?.country
+				var divisions:[String] = []
+				if let city = location?.city { divisions.append(city) }
+				if let state = location?.subdivisions?.first { divisions.append(state["name"]!) }
+				if let country = location?.country { divisions.append(country) }
+				if let country_code = location?.country_code {
+					let url = NSURL(string: "http://flagpedia.net/data/flags/normal/\(country_code.lowercaseString).png")
+					if let data = NSData(contentsOfURL: url!) {
+						self.flag!.image = UIImage(data: data)
+					}
+				}
+				self.sublabel!.text = ", ".join(divisions)
 			}
 		}
 	}
